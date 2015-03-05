4f5cfc11be4f7ba71a63ac4fe42070401f6845f2
Ned Zhang <ned_zhang@hotmail.com>
2015-03-05 10:58:03 -0600

    Updated provision_minion.sh. Added the change to /etc/kubernetes/kubelet

--- provision_minion.sh
+++ provision_minion.sh
@@ -12,6 +12,19 @@ gpgcheck=0
 yum install -y http://cbs.centos.org/kojifiles/packages/etcd/0.4.6/7.el7.centos/x86_64/etcd-0.4.6-7.el7.centos.x86_64.rpm
 yum -y install --enablerepo=virt7-testing kubernetes
 
+echo "# The address for the info server to serve on
+KUBELET_ADDRESS="--address=0.0.0.0"
+
+# The port for the info server to serve on
+KUBELET_PORT="--port=10250"
+
+# You may leave this blank to use the actual hostname
+KUBELET_HOSTNAME="--hostname_override=${MASTER_ADDR}"
+
+# Add your own!
+KUBELET_ARGS=""
+" > /etc/kubernetes/kubelet
+
 echo "# Comma separated list of nodes in the etcd cluster
 KUBE_ETCD_SERVERS=\"--etcd_servers=http://${MASTER_ADDR}:4001\"
 
